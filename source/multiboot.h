822be0550bb93c57b18d33c121f6253488f89b5d
Nick Birney <nickdab@gmail.com>
2012-04-19 12:13:33 -0500

    forgot to commit last night

--- source/multiboot.h
+++ source/multiboot.h
@@ -1,7 +1,16 @@
 #ifndef MULTIBOOT_H
 #define MULTIBOOT_H
 
-#define MULTIBOOT_MAGIC	0x1BADB002
-#define MULTIBOOT_FLAGS 0x00000000
+#define MULTIBOOT_MAGIC		0x1BADB002
+#define MULTIBOOT_FLAGS 	0x00000004	// sets bit [2] -- we have to provide info about Video Mode Table
+
+//preferred graphics mode settings:
+#define MULTIBOOT_VMODE		0x00000001	// EGA-standard text mode
+
+#define MULTIBOOT_VWIDTH	0		// we don't care
+#define MULTIBOOT_VHEIGHT 	0		// we don't care
+#define MULTIBOOT_VDEPTH	0		// we don't care
+
+
 
 #endif
